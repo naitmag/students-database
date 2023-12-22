35d64d5ee47b0513357283639c25a1b9a1793dfb
naitmag <me-yarmolovich@gmail.com>
2023-12-22 18:54:10 +0300

    new context menu for student

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -37,16 +38,16 @@ namespace Database
                 {
                     case ConsoleKey.D1:
                         CreateStudent(studbase);
-                        
+
                         break;
-                        case ConsoleKey.D2:
-                            FindStudent(studbase);
+                    case ConsoleKey.D2:
+                        FindStudent(studbase);
                         break;
-                        case ConsoleKey.D3:
-                            Remove(studbase);
+                    case ConsoleKey.D3:
+                        Remove(studbase);
                         break;
                     case ConsoleKey.D4:
-                        PrintStudentsList(studbase.GetList(), true);
+                        PrintStudentsList(studbase, studbase.GetList(), true);
                         break;
                     case ConsoleKey.F12:
                         SerializeObject("data.bin", studbase);
@@ -82,35 +83,35 @@ namespace Database
                 case ConsoleKey.D1:
                     Console.WriteLine("Введите фамилию");
                     request = Console.ReadLine();
-                    PrintStudentsList(studbase.FindByLastName(request), false);
+                    PrintStudentsList(studbase, studbase.FindByLastName(request), false);
                     break;
-                    case ConsoleKey.D2:
+                case ConsoleKey.D2:
                     Console.WriteLine("Введите имя");
                     request = Console.ReadLine();
-                    PrintStudentsList(studbase.FindByName(request), false);
+                    PrintStudentsList(studbase, studbase.FindByName(request), false);
                     break;
                 case ConsoleKey.D3:
                     Console.WriteLine("Введите группу");
                     request = Console.ReadLine();
-                    PrintStudentsList(studbase.FindByGroup(request), false);
+                    PrintStudentsList(studbase,studbase.FindByGroup(request), false);
                     break;
                 case ConsoleKey.D4:
                     Console.WriteLine("Введите название населенного пункта");
                     request = Console.ReadLine();
-                    PrintStudentsList(studbase.FindByLocation(request), false);
+                    PrintStudentsList(studbase, studbase.FindByLocation(request), false);
                     break;
                 case ConsoleKey.D5:
                     Console.WriteLine("Введите инстаграм");
                     request = Console.ReadLine();
-                    PrintStudentsList(studbase.FindByInst(request), false);
+                    PrintStudentsList(studbase, studbase.FindByInst(request), false);
                     break;
                 case ConsoleKey.D6:
                     Console.WriteLine("Введите инстаграм");
                     request = Console.ReadLine();
-                    PrintStudentsList(studbase.FindByTelegram(request), false);
+                    PrintStudentsList(studbase,studbase.FindByTelegram(request), false);
                     break;
             }
-                  
+
         }
         static void CreateStudent(StudBase database)
         {
@@ -148,15 +149,146 @@ namespace Database
                 return (T)formatter.Deserialize(fs);
             }
         }
+        static void GetStudentMenu(StudBase studentBase, Student student)
+        {
+            Console.Clear();
+            ConsoleColor color = ConsoleColor.Magenta;
+            ConsoleColor oldColor = Console.ForegroundColor;
 
-        static void PrintStudentsList(List<Student> list, bool showFullInfo)
+            Console.ForegroundColor = color;
+            Console.WriteLine("===================");
+            Console.ForegroundColor = oldColor;
+
+            student.ShowFullInfo();
+
+            Console.ForegroundColor = color;
+            Console.WriteLine("===================");
+            Console.ForegroundColor = oldColor;
+
+            Console.WriteLine("Выберите действие:\n1)Изменить студента\n2)Удалить студента");
+
+            bool getTargetKey = false;
+
+            while (!getTargetKey)
+            {
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.Escape:
+                        getTargetKey = true;
+                        break;
+                    case ConsoleKey.D2:
+                        studentBase.RemoveStudent(student.ID);
+                        getTargetKey = true;
+                        break;
+                        
+                }
+            }
+            
+        }
+
+        static void PrintStudentsList(StudBase studentBase ,List<Student> list, bool showFullInfo)
         {
             ConsoleColor color = ConsoleColor.Blue;
             ConsoleColor oldColor = Console.ForegroundColor;
-            int resultsOnPage = 2;
-            for (int i = 0; i < list.Count; i++)
+            int resultsOnPage = 3;
+            int maxPages = list.Count / resultsOnPage + (list.Count % resultsOnPage > 0 ? 1 : 0) - 1;
+            int targetPage = 0;
+
+            bool closeList = false;
+
+            var studentsOnScreen = new List<Student>();
+
+
+            while (!closeList)
             {
-                
+                Console.Clear();
+                if (targetPage > maxPages)
+                {
+                    targetPage = maxPages;
+                    Console.WriteLine("Достигнута последняя страница");
+                }
+                else if (targetPage < 0)
+                {
+                    targetPage = 0;
+                    Console.WriteLine("Достигнута первая страница");
+                }
+                Console.WriteLine($"Страницы {targetPage + 1} из {maxPages + 1}");
+                for (int j = targetPage * resultsOnPage; j < resultsOnPage * targetPage + resultsOnPage; j++)
+                {
+                    if (j >= list.Count)
+                    {
+                        break;
+                    }
+                    Console.ForegroundColor = color;
+                    Console.WriteLine("=================");
+                    Console.ForegroundColor = oldColor;
+                    list[j].ShowInfo();
+                    Console.ForegroundColor = color;
+                    Console.WriteLine("=================");
+                    Console.ForegroundColor = oldColor;
+                    studentsOnScreen.Add(list[j]);
+                }
+                bool getTargetKey = false;
+                while (!getTargetKey)
+                {
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.RightArrow:
+                            targetPage++;
+                            getTargetKey = true;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            targetPage--;
+                            getTargetKey = true;
+                            break;
+                        case ConsoleKey.D1:
+                            GetStudentMenu(studentBase,studentsOnScreen[0]);
+                            getTargetKey = true;
+                            break;
+                    }
+                }
+                studentsOnScreen.Clear();
+
+            }
+
+
+
+
+
+            /*for(int i=1;i<=list.Count/resultsOnPage+ (list.Count%resultsOnPage!=0? 1:0) ; i++)
+            {
+                for(int j=1*i;j<=1*i+resultsOnPage;j++)
+                {   
+                    if(i==list.Count / resultsOnPage+1&&j==list.Count/resultsOnPage-1)
+                    {
+                        break;
+                    }
+                    list[j-1].ShowFullInfo();
+                }
+                bool nextStep = false;
+                while (!nextStep)
+                {
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.Enter:
+                            nextStep = true;
+                            break;
+                        case ConsoleKey.Escape:
+                            nextStep = true;
+                            i = list.Count;
+                            break;
+
+
+                    }
+
+                }
+                Console.Clear();
+            }
+    */
+
+            /*for (int i = 0; i < list.Count; i++)
+            {
+
                 Console.ForegroundColor = color;
                 Console.WriteLine("======================");
                 Console.ForegroundColor = oldColor;
@@ -171,9 +303,34 @@ namespace Database
                 Console.ForegroundColor = color;
                 Console.WriteLine("======================");
                 Console.ForegroundColor = oldColor;
-                
-            }
-                    
+
+                if (i != 0 && i % resultsOnPage == 0)
+                {
+                    Console.WriteLine("Для продолжения нажмите 'Enter'.");
+
+                    bool nextStep = false;
+                    while (!nextStep)
+                    {
+                        switch (Console.ReadKey().Key)
+                        {
+                            case ConsoleKey.Enter:
+                                nextStep = true;
+                                break;
+                            case ConsoleKey.Escape:
+                                nextStep = true;
+                                i = list.Count;
+                                break;
+
+
+                        }
+
+                    }
+                    Console.Clear();
+                }
+
+            }*/
+
+
         }
     }
     [Serializable]
